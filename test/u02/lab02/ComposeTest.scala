7c5f9ad4a833c22695dfa1652128fdfeb39931f4
Andrea Negri <andrea.negri4@studio.unibo.it>
2021-03-11 17:55:21 +0100

    5) Fix nei test: i metodi venivano richiamati direttamente dalla classe e non dall'oggetto creato nei test

--- test/u02/lab02/ComposeTest.scala
+++ test/u02/lab02/ComposeTest.scala
@@ -9,17 +9,17 @@ class ComposeTest {
 
   @Test
   def testCompose(){
-    assertEquals(9, Compose.compose(_-1,_*2)(5))
+    assertEquals(9, composeObj.compose(_-1,_*2)(5))
 
-    assertNotEquals(10, Compose.compose( _+1, x => x*x)(5))
-    assertEquals(26, Compose.compose( _+1, x => x*x)(5))
+    assertNotEquals(10, composeObj.compose( _+1, x => x*x)(5))
+    assertEquals(26, composeObj.compose( _+1, x => x*x)(5))
   }
 
   @Test
   def testGenericCompose(){
     testCompose()
 
-    assertEquals("Ciao Andrea !", Compose.genericCompose[String](s2 => s2.concat(" !"),s1 => "Ciao ".concat(s1))("Andrea"))
-    assertFalse(Compose.genericCompose[Boolean](b2 => b2 && true, b1 => !b1)(true))
+    assertEquals("Ciao Andrea !", composeObj.genericCompose[String](s2 => s2.concat(" !"),s1 => "Ciao ".concat(s1))("Andrea"))
+    assertFalse(composeObj.genericCompose[Boolean](b2 => b2 && true, b1 => !b1)(true))
   }
 }
